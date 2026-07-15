a60304af1a7f3efbe1718a06b1597635df29dfb2
Steph3nnn <ernieochai2008@gmail.com>
2026-07-15 07:13:51 -0400

    Inital Spring Boot Project setup

---
+++ MedFlow/src/main/java/com/example/MedFlow/MedFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedFlowApplication.class, args);
+	}
+
+}
---
+++ MedFlow/src/test/java/com/example/MedFlow/MedFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MedFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
