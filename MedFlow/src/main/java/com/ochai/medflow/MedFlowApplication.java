dc8c5e62e67c9f15504a0f1ddfd1155151816acd
Steph3nnn <ernieochai2008@gmail.com>
2026-07-15 10:26:39 -0400

    refactor: base package updated to com.ochai.medflow

--- MedFlow/src/main/java/com/ochai/medflow/MedFlowApplication.java
+++ MedFlow/src/main/java/com/ochai/medflow/MedFlowApplication.java
@@ -1,4 +1,4 @@
-package com.example.MedFlow;
+package com.ochai.medflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- MedFlow/src/test/java/com/ochai/medflow/MedFlowApplicationTests.java
+++ MedFlow/src/test/java/com/ochai/medflow/MedFlowApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.MedFlow;
+package com.ochai.medflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
