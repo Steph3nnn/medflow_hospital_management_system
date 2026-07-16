416487e498746bf48229fda5c11c863fe065d295
Steph3nnn <ernieochai2008@gmail.com>
2026-07-16 05:07:28 -0400

    feat(auth): implement User entity

---
+++ MedFlow/src/main/java/com/ochai/medflow/authentication/entity/User.java
@@ -0,0 +1,37 @@
+package com.ochai.medflow.authentication.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.apache.logging.log4j.message.StringFormattedMessage;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean enabled = true;
+}
