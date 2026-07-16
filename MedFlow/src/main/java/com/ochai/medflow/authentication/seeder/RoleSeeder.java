bf4c38ba291813c8366bb1c77299ae1c78db8b24
Steph3nnn <ernieochai2008@gmail.com>
2026-07-16 06:11:35 -0400

    role seeder updated

--- MedFlow/src/main/java/com/ochai/medflow/authentication/seeder/RoleSeeder.java
+++ MedFlow/src/main/java/com/ochai/medflow/authentication/seeder/RoleSeeder.java
@@ -4,28 +4,19 @@ import com.ochai.medflow.authentication.entity.Role;
 import com.ochai.medflow.authentication.repository.RoleRepository;
 import com.ochai.medflow.common.enums.RoleName;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class RoleSeeder implements CommandLineRunner {
+public class RoleSeeder implements ApplicationRunner {
 
     private final RoleRepository roleRepository;
 
     @Override
-    public void run(String... args) {
-
-        for (RoleName roleName : RoleName.values()) {
-
-            if (roleRepository.findByName(roleName).isEmpty()) {
-
-                Role role = Role.builder()
-                        .name(roleName)
-                        .build();
-
-                roleRepository.save(role);
-            }
-        }
+    public void run(ApplicationArguments args) {
+        // Seed roles here
     }
 }
