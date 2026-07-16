beaae724fcbca6718b079af332a19dda90fcd6a6
Steph3nnn <ernieochai2008@gmail.com>
2026-07-16 06:01:50 -0400

    feat(auth): security integrated

--- MedFlow/src/main/java/com/ochai/medflow/authentication/entity/User.java
+++ MedFlow/src/main/java/com/ochai/medflow/authentication/entity/User.java
@@ -2,7 +2,13 @@ package com.ochai.medflow.authentication.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.apache.logging.log4j.message.StringFormattedMessage;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
@@ -11,7 +17,7 @@ import org.apache.logging.log4j.message.StringFormattedMessage;
 @AllArgsConstructor
 @Builder
 @Table(name = "users")
-public class User {
+public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -34,4 +40,34 @@ public class User {
 
     @Column(nullable = false)
     private boolean enabled = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.getName().name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
