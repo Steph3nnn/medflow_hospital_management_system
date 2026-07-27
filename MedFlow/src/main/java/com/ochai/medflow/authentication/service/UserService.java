26a24ad355bbe6559138ba95ac549d2f5a5325fb
Steph3nnn <ernieochai2008@gmail.com>
2026-07-27 08:26:24 -0400

    feat(admin): user editing finalized

--- MedFlow/src/main/java/com/ochai/medflow/authentication/service/UserService.java
+++ MedFlow/src/main/java/com/ochai/medflow/authentication/service/UserService.java
@@ -35,7 +35,6 @@ public class UserService {
         user.setFirstName(updatedUser.getFirstName());
         user.setLastName(updatedUser.getLastName());
         user.setEmail(updatedUser.getEmail());
-        user.setRole(updatedUser.getRole());
 
         userRepository.save(user);
     }
