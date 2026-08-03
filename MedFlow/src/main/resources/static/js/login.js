1741a3b6e7c243a323845a08784226ef2201da50
Steph3nnn <ernieochai2008@gmail.com>
2026-08-03 04:55:51 -0400

    feat(auth): update login page

---
+++ MedFlow/src/main/resources/static/js/login.js
@@ -0,0 +1,20 @@
+const togglePassword =
+    document.getElementById("togglePassword");
+
+const password =
+    document.getElementById("password");
+
+togglePassword.addEventListener("click", function () {
+
+    const type =
+        password.getAttribute("type") === "password"
+            ? "text"
+            : "password";
+
+    password.setAttribute("type", type);
+
+    this.innerHTML =
+        type === "password"
+            ? '<i class="bi bi-eye-fill"></i>'
+            : '<i class="bi bi-eye-slash-fill"></i>';
+});
